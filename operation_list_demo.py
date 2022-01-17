efda30be55a99ef99efe3b9801ae8abfdccab9d1
opmin123 <7316128599@qq.com>
2022-01-17 22:03:50 +0800

    操作列表demo
    Signed-off-by: opmin123 <7316128599@qq.com>

--- operation_list_demo.py
+++ operation_list_demo.py
@@ -3,4 +3,10 @@
 #操作列表demo
 magicians = ['alice','david','carolina']
 for mag in magicians:
-    print(mag)
+  print(mag.title() + ",that was a great tricek")
+#demo2
+print("=================================== 第二个demo ==================================")
+pizzas = ['pizz_1','pizz_2','pizz_3']
+for pizza in pizzas:
+    print(pizza + ',I like pepperoni pizza!')
+print("I really love pizza!")
